0ee037c58c3046c3b6d2cf33deabf10d40ebe1d9
MWaheed62 <alamatishh@gmail.com>
2022-10-24 12:56:05 +0500

    conditional variable

---
+++ fist_chilla/05_variables.py
@@ -0,0 +1,27 @@
+# Variables: objects containing specific value
+x=45  #intger or numeric variable
+print(x)
+
+y="what is your name?"
+print(y)
+z=4.5
+print(type(x)) #type or class of variable
+print(type(y)) #string variable
+print(type(z)) #float variable
+x=x+6
+print(x) # variable update by up to down line
+
+
+#rules to assign a variable:
+# 1- The variables should contain letters, numbers or underscores
+# 2- Do not start with numbes
+# 3- Spaces are not allowed
+# 4- Do not use keywords used in functions ( break, media, if els, test etc)
+# 5- Variable should be short and descriptive
+# 6- Variables are case sensetive ( use lower case )
+
+
+fruit_baskit= "mangoes, oranges, bananas"
+print(fruit_baskit)
+shoe_box= 3
+print(shoe_box)
