5969e3edff6df01576d74fd3fe0da98d457325ef
MWaheed62 <alamatishh@gmail.com>
2022-10-24 12:43:38 +0500

    input variables in python

---
+++ fist_chilla/06_input_variables.py
@@ -0,0 +1,22 @@
+#input function simple stage
+
+# favourit_colour= input("What is your favourit colour? ")
+# print(favourit_colour)
+
+# #input function 2nd stage
+
+# name= input("What is your name? ")
+# greetings= "Hello!"
+# print(greetings, name)
+
+#input function , another way of 2nd stage
+
+# name= input("What is your name? ")
+# print("Hello!", name)
+
+#input function 3rd stage
+
+# name= input("What is your name? ")
+# age= input("how old are you? ")
+# greetings= "Hello!"
+# print(greetings,name, ", you are still young")
