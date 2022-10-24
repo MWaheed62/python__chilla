259f2fc062face8cb979305f74c697fb1d5151c5
MWaheed62 <alamatishh@gmail.com>
2022-10-24 12:53:19 +0500

    conditonal logics

---
+++ fist_chilla/07_conditonal_logics.py
@@ -0,0 +1,25 @@
+#logical operators are either "true or false" or "yes or no" or "0 or 1"
+#equal to                           ==
+#not equal to                       !=
+#less than                          <
+#greater than                       >
+#less than and equal to             <=
+#greater than and equal to          >=
+
+#is 5 greater than 3?
+# print(5>3)
+# print(5<3)
+# print(4==5)
+# print(10==9+1)
+# print(7<=22)
+# print(6!=5-2)
+
+#applications of logical operators
+
+Saif_age=15
+netflex_age= 18
+print(Saif_age==netflex_age)
+
+Saif_age=24
+netflex_age= 18
+print(Saif_age>=netflex_age)
